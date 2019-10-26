1417f13c5e6178e2773ed71e9152a958778acfb3
markrajk <marko88rajkovic@gmail.com>
2019-10-26 11:39:10 +0200

    connected to db

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,6 +25,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`);
+const server = app.listen(PORT, () => {
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`.yellow.bold);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejections', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Cloese server & exit process
+  server.close(() => process.exit(1));
 });
