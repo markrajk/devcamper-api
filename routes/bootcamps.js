6832779b2c47cf43d4e9af7b12ae220aace5c9ab
markrajk <marko88rajkovic@gmail.com>
2019-10-27 12:36:41 +0100

    added course routes, photo upload

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,12 +5,21 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
-  getBootcampsInRadius
+  getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require('../controllers/bootcamps');
+
+// Include other ressource routers
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 
